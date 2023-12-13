9dd681617d12abd047081b1245570d339fc0a212
Musab-Dev <musab.dev98@gmail.com>
2023-12-13 19:55:39 +0300

    [style] books/index view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts.app')
 
+@section('page-actions')
+    <a class="page-action-link" href="{{ route('books.create') }}">+ Add New Book</a>
+@endsection
+
 @section('title', 'All Books!')
 
 @section('content')
 
     @forelse ($books as $book)
-        <div>{{ $book->title }}</div>
+        <a href="{{ route('books.show', ['book' => $book]) }}">
+            <div class="book-card">
+                <div class="mb-3">
+                    <p class="book-isbn">
+                        <strong class="font-bold">ISBN: </strong> {{ $book->isbn }}
+                    </p>
+                </div>
+                <div class="book-title">
+                    <p>{{ $book->title }}</p>
+                </div>
+                <div class="ml-4">
+                    <p class="book-author">{{ $book->author }}</p>
+                </div>
+            </div>
+        </a>
     @empty
         <div>there are no books yet! Try Add One</div>
     @endforelse
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,52 @@
 
 <head>
     <title></title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <style type="text/tailwindcss">
+        .page-actions {
+            @apply my-4
+        }
+
+        .page-action-link {
+            @apply text-sm font-normal p-2 mr-2 border border-slate-300 hover:border-slate-700 rounded transition
+        }
+
+        .page-title {
+            @apply text-2xl font-bold mb-5
+        }
+
+        .book-card {
+            @apply bg-gray-50 border border-gray-100 rounded-lg drop-shadow-lg mb-5 px-4 py-6
+        }
+
+        .book-isbn {
+            @apply inline bg-gray-100 border border-slate-300 rounded-lg px-2 py-1 text-sm font-light text-gray-600
+        }
+
+        .book-title {
+            @apply text-lg font-bold
+        }
+
+        .book-author {
+            @apply text-sm font-light text-gray-700
+        }
+    </style>
 </head>
 
 <body>
-    <h1>
-        @yield('title')
-    </h1>
-    <div>
-        @yield('content')
+    <div class="container mt-6 m-auto">
+        <div class="page-actions">
+            @yield('page-actions')
+        </div>
+        <h1 class="page-title">
+            @yield('title')
+        </h1>
+        <div>
+            @yield('content')
+        </div>
     </div>
+
 </body>
 
 </html>
